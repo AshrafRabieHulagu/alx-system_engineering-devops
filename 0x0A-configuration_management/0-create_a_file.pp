19e2ae39dc3a42fd95eb737dfb25fc5106217b85
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-20 05:21:06 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
-# cerates a file
-file { '/tmp/school';
-        mode    => '0744',
-        owner   => 'www-data',
-        group   => 'www-data',
-        content => 'I love puppet'
+#The code will create a file name school inside the /tmp Directory
+file { '/tmp/school':
+        ensure  =>  file,
+        content =>  'I love Puppet',
+	mode    =>  '07744',
+	owner   =>  'www-data',
+	group   =>  'www-data',
 }
