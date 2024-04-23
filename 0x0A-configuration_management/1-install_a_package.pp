2fbf19f790dbc443659c425233000c55da0835a0
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-23 01:31:52 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,9 +1,9 @@
 package { 'python3-pip':
-  ensure => installed,
+ensure => installed,
 }
 
 exec { 'install_flask':
-  command => '/usr/bin/pip3 install Flask==2.1.0',
-  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
-  require => Package['python3-pip'],
+command => '/usr/bin/pip3 install Flask==2.1.0',
+unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
+require => Package['python3-pip'],
 }
