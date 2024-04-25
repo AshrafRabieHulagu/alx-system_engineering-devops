26caf78f618a66d665cae8cef6b81529d92838b9
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-04-25 07:30:42 -0700

    0x0A. Configuration management

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,11 @@
-#This code will install the package puppet-lint
-package {'flask':
-  ensure   => '2.1.0',
-  provider => 'pip'
+# Ensure python3-pip package is installed
+package { 'python3-pip':
+  ensure => installed,
+}
+
+# Install Flask using pip3 with the specified version
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
