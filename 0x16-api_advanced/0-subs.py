05bf3af5602a7b87d23cf4befa40c8a213c82263
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-18 19:53:26 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,27 @@
 #!/usr/bin/python3
 """
-contains the number_of_subscribers function
+Function that queries the Reddit API and returns
+the number of subscribers for a given subreddit.
 """
-
 import requests
+import sys
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscriberes for a given subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
+    """ Queries to Reddit API """
+    u_agent = 'Mozilla/5.0'
+
+    headers = {
+        'User-Agent': u_agent
+    }
+
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    if res.status_code != 200:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': 'Python/requests:APIproject:\
-    v1.0.0 (by /u/fraol21)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+    dic = res.json()
+    if 'data' not in dic:
+        return 0
+    if 'subscribers' not in dic.get('data'):
+        return 0
+    return res.json()['data']['subscribers']
