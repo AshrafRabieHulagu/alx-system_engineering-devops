3398b1f8eeddf4e64cd6b27125bf496ab6de2a74
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 15:53:18 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,18 +1,19 @@
 #!/usr/bin/python3
-"""
-doc for module
-"""
+'''queries and returns corresponding result from the Reddit API'''
 import requests
-
-headers = {"User-Agent": "MyCustomUserAgent/1.0"}
 
 
 def number_of_subscribers(subreddit):
-    """method doc"""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, allow_redirects=False, headers=headers)
-    if response.status_code == 200:
-        data = response.json()
-        return data["data"]["subscribers"]
-    else:
+    '''
+    Description:
+    - queries and returns the number of subscibers
+      (not active users, total subscribers) for a given subreddit.
+    - returns 0 if an invalid subreddit is given
+    '''
+    endpoint = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'My User Agent 1.0'}
+    res = requests.get(endpoint, headers=headers)
+
+    if res.status_code != 200:
         return 0
+    return res.json()['data']['subscribers']
