cdd4d29eb6e4457051c8c4e67750e80884bab5cc
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-18 18:29:12 -0700

    0x16. API advanced

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+the project 0x16. API advanced soultion
+Submitted by: Ashraf Rabie Hulagu
+
+"""
+    How many subs
+"""
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """
+    returns the number of subscribers
+
+    Args:
+        subreddit (string): Particular topic
+    """
+    url = 'https://api.reddit.com/r/{}/about'.format(subreddit)
+    headers = {'User-Agent': 'Alien'}
+    response = requests.get(url, headers=headers).json()
+    try:
+        return response.get('data').get('subscribers')
+    except Exception:
+        return 0
