27656480668310ee38b3851c152270ef50341bbb
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 08:58:45 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,48 @@
 #!/usr/bin/python3
-"""module that queries the Reddit API and returns the number of subscribers"""
+"""
+Queries the Reddit API and returns the number
+of subscribers for a given subreddit.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function that  queries the Reddit API and
-    returns the number of subscribers)"""
-    link = 'https://www.reddit.com/r/{}/about.json'.format(str(subreddit))
-    responce = requests.get(link, headers={'User-Agent': 'raafat'})
-    if responce.status_code == 200:
-        data = responce.json()
-        return data.get('data').get('subscribers')
+    """
+    Retrieve the number of subscribers for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        int: The number of subscribers for the subreddit,
+        or 0 if the subreddit is invalid.
+    """
+    # Set a custom User-Agent to avoid Too Many Requests errors
+    headers = {'User-Agent': 'MyRedditBot/1.0'}
+
+    # Define the API endpoint URL
+    url = f'https://www.reddit.com/r/{subreddit}/about.json'
+
+    # Send an HTTP GET request to the Reddit API
+    response = requests.get(url, headers=headers)
+
+    # Check if the request was successful (status code 200)
+    if response.status_code == 200:
+        # Parse the JSON response and extract the number of subscribers
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
     else:
+        # Invalid subreddit or other error, return 0
         return 0
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        subreddit = sys.argv[1]
+        subscribers = number_of_subscribers(subreddit)
+        print(subscribers)
