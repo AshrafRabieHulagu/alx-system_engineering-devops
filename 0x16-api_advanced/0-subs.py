80734feefafe3466c2ffe0ac8f05cc1e9d233554
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 15:55:58 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-"""Script that returns no. of subscribers of a subreddit"""
+"""
+    Uses the reddit API for get the all hot posts
+"""
 import requests
+from sys import argv
 
 
-def number_of_subscribers(subreddit):
-    """Function that returns no. of subs of a subreddit"""
-    if subreddit is None or not isinstance(subreddit, str):
-        return 0
+def recurse(subreddit, hot_list=[], after=""):
+    """Get the all host posts"""
+    if after is None:
+        return []
 
-    headers = {'User-Agent': 'selBot/1.0'}
-    URL = f'https://www.reddit.com/r/{subreddit}/about.json'
-
-    try:
-        response = requests.get(URL, headers=headers, allow_redirects=False)
-        response.raise_for_status()
-        data = response.json()
-        subscribers = data['data']['subscribers']
-        return subscribers
-
-    except requests.exceptions.RequestException:
-        return 0
-
-    except (KeyError, ValueError):
-        return 0
+    url_sred_inf = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    url_sred_inf += "?limit=100&after={}".format(after)
+    headers = {'user-agent': 'request'}
+    response = requests.get(url_sred_inf, headers=headers,
+                            allow_redirects=False)
+    if str(response) != "<Response [200]>":
+        return None
+    r_json = response.json()
+    hot_posts_json = r_json.get("data").get("children")
+    for post in hot_posts_json:
+        hot_list.append(post.get("data").get("title"))
+    return hot_list + recurse(subreddit, [], r_json.get("data").get("after"))
