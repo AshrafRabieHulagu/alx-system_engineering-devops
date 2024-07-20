e54150163a7fd15334d97f0c436c85e06ad37ef5
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 17:04:46 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,45 +1,17 @@
 #!/usr/bin/python3
-""" Queries Reddit API for number of subscribers in given subreddit
-    Returns 0 if invalid subreddit
 """
-import requests as r
-import requests.auth as ra
-from sys import argv
+Contains the number_of_subscribers function
+"""
+
+import requests
 
 
 def number_of_subscribers(subreddit):
-
-    # Generates token & bearer for requests to oauth.reddit.com endpoints
-    # https://github.com/reddit-archive/reddit/wiki/OAuth2-Quick-Start-Example
-
-    client_id = "i_m3aNLQE0vskA"
-    secret = "UykV3Qz2TGSWSWaMmSeOWgR5JT4"
-    my_username = "causticpop"
-    my_pwd = "Holberton98"
-
-    client_auth = ra.HTTPBasicAuth(client_id, secret)
-    post_data = {"grant_type": "password", "username":
-                 my_username, "password": my_pwd}
-    headers = {"User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
-    response = r.post("https://www.reddit.com/api/v1/access_token",
-                      auth=client_auth, data=post_data, headers=headers)
-    token = response.json().get("access_token")
-    bearer = response.json().get("token_type")
-
-    # Queries oauth.reddit.com endpoints using token
-
-    subreddit = argv[1]
-    sub_url = "https://oauth.reddit.com/r/{}/about".format(subreddit)
-
-    headers = {"Authorization": "{} {}".format(bearer, token),
-               "User-Agent": "ChangeMeClient/0.1 by {}".format(my_username)}
-    response = r.get(sub_url, headers=headers)
-
-    # handles error response; invalid subreddit
-    if response.status_code is not 200:
-        return(0)
-    else:
-        response_json = response.json().get('data')
-        result = response_json.get('subscribers')
-
-        return result
+    """returns the number of subscribers for a given subreddit"""
+    if subreddit is None or type(subreddit) is not str:
+        return 0
+    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
+                     headers={'User-Agent': '0x16-api_advanced:project:\
+v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
+    subs = r.get("data", {}).get("subscribers", 0)
+    return subs
