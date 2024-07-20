4082762ce8095aa57de72b01b9024c14b18b7600
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 17:13:58 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,43 +1,23 @@
 #!/usr/bin/python3
-""" function that queries the Reddit API """
+"""
+function that queries the Reddit API.
+"""
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """ function that queries the Reddit API and returns the number of
-        subscribers (not active users, total subscribers) for a given
-        subreddit. If an invalid subreddit is given, the function returns 0
-    """
-    CLIENT_ID = 'zL8evAsF03DzVk_TArVH3g'
-    SECRET_KEY = 'FT7BTQs-WLEtZEOlH_2mE2QEQ8CV2g'
+    """URL for the subreddit's about.json endpoint"""
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    auth = requests.auth.HTTPBasicAuth(CLIENT_ID, SECRET_KEY)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
-    data = {
-        'grant_type': 'password',
-        'username': 'Responsible-Big4336',
-        'password': 'password123'
-    }
-
-    headers = {'User-Agent': 'MyAPI1'}
-
-    res = requests.post('https://www.reddit.com/api/v1/access_token',
-                        auth=auth, data=data, headers=headers)
-
-    TOKEN = res.json()['access_token']
-
-    headers['Authorization'] = "bearer {}".format(TOKEN)
-
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    response = requests.get(url, auth=auth, data=data, headers=headers,
-                            allow_redirects=False)
-
-    if response.status_code != 200:
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        else:
+            return 0
+    except requests.RequestException:
         return 0
-    d = response.json()
-    if 'data' not in d:
-        return 0
-    if 'subscribers' not in d.get('data'):
-        return 0
-
-    return d['data']['subscribers']
