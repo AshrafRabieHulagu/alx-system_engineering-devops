8dd3a4322e1bcc84f03f408da9c2fce24144a1e6
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-07-19 17:19:53 -0700

    0x16. API advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
 """
-returns the number of subscribers (not active users, total subscribers)
+This module contains a function to query the Reddit API for
+the number of subscribers in a given subreddit.
 """
+
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers of a subreddit"""
-    if subreddit is None:
-        return (0)
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'user-agent': 'API Project by andreshugueth'}
-    r = requests.get(url, headers=headers).json()
-    subscribers = r.get("data", {}).get("subscribers", 0)
-    return subscribers
+    """
+    Queries the Reddit API and returns the number of subscribers
+    for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {'User-Agent': 'MyRedditAPI/0.1'}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code != 200:
+        return 0
+
+    data = response.json().get('data', {})
+    return data.get('subscribers', 0)
