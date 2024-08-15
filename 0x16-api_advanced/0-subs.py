e6abf0568767e3c44a8c60566545a395ffe76945
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 06:35:34 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-"""Count subscribers"""
+"""subs module"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Returns number of subscribers in a reddit"""
+    """
+    Gets the numbers of subscribers in a subreddit
+    """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Mozilla/5.0'}
 
-    api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {'User-Agent': 'Alx-Project by jude'}
-    response = requests.get(api, headers=headers, allow_redirects=False)
-
+    response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
-        record = response.json().get('data')
-        return record.get('subscribers')
-    return 0
+        print(response.json()['data']['subscribers'])
+    else:
+        return 0
