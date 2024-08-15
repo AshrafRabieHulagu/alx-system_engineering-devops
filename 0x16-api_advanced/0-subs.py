1e69ac0b6bf732a6575e3d997f300e492b04a853
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 09:26:59 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,18 @@
 #!/usr/bin/python3
-"""A function that queries the Reddit API and returns the number of subscribers
-(not active users, total subscribers) for a given subreddit.
-"""
-
+""" queries the Reddit API """
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Get total subscriber of a given subreddit"""
+    """ returns the number of subscribers for a given subreddit. """
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-
-    headers = requests.utils.default_headers()
-    """Change client's User-Agent string to something unique and descriptive
-    as required by Reddit API terms"""
-    headers.update({'User-Agent': 'Temz User Agent version 1.0'})
-    r = requests.get(url, headers=headers).json()
-    t_subscriber = r.get("data", {}).get("subscribers")
-
-    if not t_subscriber:
+    headers = {
+        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        data = response.json().get('data')
+        subscribers = data.get('subscribers')
+        return subscribers
+    except (KeyError, requests.RequestException):
         return 0
-    else:
-        return t_subscriber
