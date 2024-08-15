ca4ce3ac439d8838ed77e64eee7c63b3ddc137a8
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 10:59:13 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-"""
-Contains the number_of_subscribers function
-"""
+"""Module for querying the Reddit API and retrieving."""
 
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """returns the number of subscribers for a given subreddit"""
-    if subreddit is None or type(subreddit) is not str:
+    """
+    Query the Reddit API for subreddit information.
+
+    Args:
+        subreddit (str): The name of the subreddit to retrieve information.
+
+    Returns:
+        int: The number of subscribers to the subreddit, or 0 if the subreddit.
+    """
+    # Construct the URL to request subreddit information
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+
+    # Set custom headers with a User-Agent
+    headers = {"User-Agent": "My-User-Agent"}
+
+    # Send a GET request to the Reddit API's subreddit information endpoint
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    # Check if the HTTP status code indicates an error (status code >= 300)
+    if response.status_code >= 300:
         return 0
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit),
-                     headers={'User-Agent': '0x16-api_advanced:project:\
-v1.0.0 (by /u/firdaus_cartoon_jr)'}).json()
-    subs = r.get("data", {}).get("subscribers", 0)
-    return subs
+
+    # Extract the number of subscribers from the JSON response
+    return response.json().get("data").get("subscribers")
