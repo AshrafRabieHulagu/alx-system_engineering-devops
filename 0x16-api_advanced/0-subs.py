7d8254f520ff394706e071b8ccec75cd13ddb9ad
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 09:49:16 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,58 +1,27 @@
 #!/usr/bin/python3
 """
-Module for count_words function
+number of subscribers for a given subreddit
 """
-import requests
+
+from requests import get
 
 
-def count_words(subreddit, word_list, new_after='',
-                words_dict={}):
+def number_of_subscribers(subreddit):
     """
-    A recursive function that queries the Reddit API,
-    parses the title of all hot articles, and prints a
-    sorted count of given keywords
+    function that queries the Reddit API and returns the number of subscribers
+    (not active users, total subscribers) for a given subreddit.
     """
 
-    word_list = map(lambda x: x.lower(), word_list)
-    word_list = list(word_list)
+    if subreddit is None or not isinstance(subreddit, str):
+        return 0
 
-    res = requests.get("https://www.reddit.com/r/{}/hot.json"
-                       .format(subreddit),
-                       headers={'User-Agent': 'Custom'},
-                       params={'after': new_after},
-                       allow_redirects=False)
-
-    if res.status_code != 200:
-        return
+    user_agent = {'User-agent': 'Google Chrome Version 81.0.4044.129'}
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    response = get(url, headers=user_agent)
+    results = response.json()
 
     try:
-        response = res.json().get('data', None)
+        return results.get('data').get('subscribers')
 
-        if response is None:
-            return
-    except ValueError:
-        return
-
-    children = response.get('children', [])
-
-    for post in children:
-        title = post.get('data', {}).get('title', '')
-        for key_word in word_list:
-            for word in title.lower().split():
-                if key_word == word:
-                    words_dict[key_word] = words_dict.get(key_word, 0) + 1
-
-    new_after = response.get('after', None)
-
-    if new_after is None:
-        sorted_dict = sorted(words_dict.items(),
-                             key=lambda x: x[1],
-                             reverse=True)
-
-        for i in sorted_dict:
-            if i[1] != 0:
-                print("{}: {}".format(i[0], i[1]))
-        return
-
-    return count_words(subreddit, word_list,
-                       new_after, words_dict)
+    except Exception:
+        return 0
