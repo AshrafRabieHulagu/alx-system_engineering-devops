b42a2ba0442687e4ad47bf04379129f9f499a13b
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 06:34:22 -0700

    0x16-api_advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-"""Function to query subscribers on a given Reddit subreddit."""
+"""Count subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the total number of subscribers on a given subreddit."""
-    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code == 404:
-        return 0
-    results = response.json().get("data")
-    return results.get("subscribers")
+    """Returns number of subscribers in a reddit"""
+
+    api = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': 'Alx-Project by jude'}
+    response = requests.get(api, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        record = response.json().get('data')
+        return record.get('subscribers')
+    return 0
