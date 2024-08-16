86a48ae7343249b5a5ac492f510933d26b61a356
AshrafRabieHulagu <ashraf.rabie.hulagu@gmail.com>
2024-08-15 22:48:50 -0700

    0x17-web_stack_debugging_3

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
-# fixes a file
+# fix  file
 exec {'replaces wrong php filetype':
   command => 'sed -i "s/.phpp/.php/g" /var/www/html/wp-settings.php',
   path    => '/bin/:/sbin/:/usr/bin/:/usr/sbin/',
